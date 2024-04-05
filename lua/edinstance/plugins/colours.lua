27dbd8b97a713eaa3a84eb7adf1ac164e48080c6
edinstance <edinstance@gmail.com>
2024-04-05 13:57:28 +0100

    feat: updated neovim config

---
+++ lua/edinstance/plugins/colours.lua
@@ -0,0 +1,11 @@
+
+
+return {
+    "navarasu/onedark.nvim",
+    config = function()
+        require("onedark").setup {
+            style = 'warm',
+        }
+        require('onedark').load()
+    end
+}
